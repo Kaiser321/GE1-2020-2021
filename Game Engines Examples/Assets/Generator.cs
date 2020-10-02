4b9567cdc6ee77fd7254833245024eb3a5bee7bd
Kai O'Leary <c15354951@mydit.ie>
2020-10-02 18:35:47 +0100

    Lab 2

---
+++ Game Engines Examples/Assets/Generator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public int numOfRings;
+    public GameObject hex_prefab;
+    public int rotationSpeed;
+    private ArrayList hexList = new ArrayList();
+    private float radius = 1.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int ring = 1; ring <= numOfRings; ring++)
+        {
+            int numOfHex = ring * 6;
+            for (int h = 0; h < numOfHex; h++)
+            {
+                float radian = 2f * (Mathf.PI / numOfHex) * h;
+
+                float xPos = Mathf.Sin(radian) * radius + transform.position.x;
+                float yPos = Mathf.Cos(radian) * radius + transform.position.y;
+
+                GameObject hex = Instantiate(hex_prefab, new Vector3(xPos, yPos, 1), new Quaternion(0, 0, 0, 0));
+                hexList.Add(hex);
+            }
+            radius += 1.25f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject hex in hexList)
+        {
+
+            hex.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+
+    }
+}
